770f90940bf6c749330e0a17304ff019b0a6396e
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2021-01-25 09:27:09 +0100

    Finished SSJS Snippet article on _MobileAddress usage

--- docs/docsSidebar.js
+++ docs/docsSidebar.js
@@ -14,6 +14,7 @@ module.exports = {
                 {
                     'SSJS Use Cases': ['usecase/engage-with-countdown', 'usecase/tailor-with-data'],
                 },
+                { 'SSJS Snippets': ['ssjs/ssjs-snippet-mobileconnect-phone-change'] },
             ],
         },
         {
