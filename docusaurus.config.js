34f745b06e4af234a1074b79f8bd7ae4dd0575e6
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2021-03-10 22:29:52 +0100

    Clean up of contextual search for Algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -162,7 +162,6 @@ module.exports = {
         algolia: {
             apiKey: '3fbd31ec489532e5020df55b3410e6e6',
             indexName: 'mateuszdabrowski',
-            contextualSearch: true,
             placeholder: 'Search Docs & Snipptes',
         },
     },
