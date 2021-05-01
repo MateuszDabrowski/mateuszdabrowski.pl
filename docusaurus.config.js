c139e1e1386f6ecf257c87235f72acd566ec556e
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2021-05-01 12:10:28 +0200

    Bump Docusaurus to alpha 75 + addition of IdealImage plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -181,6 +181,15 @@ module.exports = {
             },
         ],
         [
+            '@docusaurus/plugin-ideal-image',
+            {
+                quality: 70,
+                max: 1030, // max resized image's size.
+                min: 640, // min resized image's size. if original is lower, use that size.
+                steps: 2, // the max number of images generated between min and max (inclusive)
+            },
+        ],
+        [
             '@docusaurus/plugin-client-redirects', // Works only on production
             {
                 redirects: [
