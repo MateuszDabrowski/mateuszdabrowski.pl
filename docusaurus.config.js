6eb717fdbbe1beb098063ec2d8920fffe6f8d069
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2021-11-09 00:07:00 +0100

    Promo banner for Salesforce MVP 2022

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,13 +34,13 @@ module.exports = {
             { name: 'theme-color', content: '#212121' },
             { name: 'msapplication-TileColor', content: '#F6B355' },
         ],
-        // announcementBar: {
-        //     id: 'announcementBar',
-        //     content:
-        //         '🚧 Search currently doesn\'t work - check full list of docs & articles <a href="https://mateuszdabrowski.pl/docs/">here</a> 🚧',
-        //     backgroundColor: '#F6B355',
-        //     textColor: '#00001F',
-        // },
+        announcementBar: {
+            id: 'announcementBar',
+            content:
+                '☁️  <a href="https://nominations.salesforcemvps.com/s/" style="text-decoration: none">Do you know someone in the #TrailblazerCommunity who goes above and beyond to help others? Nominations for the 2022 Salesforce MVP class are <span style="text-decoration:underline">open</span></a> ☁️',
+            backgroundColor: '#0176d3',
+            textColor: '#fffffe',
+        },
         navbar: {
             logo: {
                 alt: 'Mateusz Dąbrowski Logo',
