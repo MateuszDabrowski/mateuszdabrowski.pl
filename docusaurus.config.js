338910e0b206aaf03c38a2b261e18e01be09a3b3
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2022-02-26 21:56:36 +0100

    Testing fix to Saerch Page lack of results with contextualSearch config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -164,7 +164,7 @@ module.exports = {
             apiKey: 'd7932184e92b94d052fab9cea784b13f',
             indexName: 'mateuszdabrowski',
             placeholder: 'Search Docs & Snipptes',
-            contextualSearch: false,
+            contextualSearch: true,
         },
     },
     presets: [
