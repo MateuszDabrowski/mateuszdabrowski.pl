4f29a1835d0c2816e3e31b3256068bac1d04466a
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2022-08-03 09:00:19 +0200

    Button component now has attribute for dedicing where the link should be opened

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
 
-const Button = ({ link, text }) => (
+const Button = ({ link, text, newTab = false }) => (
     <>
-        <a class="button button--outline button--lg button--primary col col--6 col--offset-3" href={link}>{text}</a>
+        <a class="button button--outline button--lg button--primary col col--6 col--offset-3" href={link} target={newTab ? '_blank' : '_self'}>{text}</a>
     </>
 );
 
-const ButtonRow = ({ linkLeft, textLeft, linkRight, textRight }) => (
+const ButtonRow = ({ linkLeft, textLeft, newTabLeft = false, linkRight, textRight, newTabRight = false}) => (
     <>
         <div class="col col--12">
-            <a class="button button--outline button--lg button--primary col col--5 col--offset-0" href={linkLeft}>{textLeft}</a>
-            <a class="button button--outline button--lg button--primary col col--5 col--offset-2" href={linkRight}>{textRight}</a>
+            <a class="button button--outline button--lg button--primary col col--5 col--offset-0" href={linkLeft} target={newTabLeft ? '_blank' : '_self'}>{textLeft}</a>
+            <a class="button button--outline button--lg button--primary col col--5 col--offset-2" href={linkRight} target={newTabRight ? '_blank' : '_self'}>{textRight}</a>
         </div>
     </>
 );
