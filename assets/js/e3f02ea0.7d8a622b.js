7a61ceb9bbe1f3da118faad76b9d3e1094b128c8
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2023-12-13 23:16:24 +0100

    Deploy website - based on 823740c077c4859156a182401a11edcb01860a24

---
+++ assets/js/e3f02ea0.7d8a622b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmd=self.webpackChunkmd||[]).push([[9379],{6791:e=>{e.exports=JSON.parse('{"label":"Best Practice","permalink":"/sites/tags/best-practice","allTagsPath":"/sites/tags","count":1,"items":[{"id":"newsletter","title":"Newsletter","description":"Get yet another email in your inbox. This time, about SFMC.","permalink":"/sites/newsletter"}]}')}}]);
